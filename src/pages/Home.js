7695f029d0f92da48c8862d1ecd5740be2074304
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-06-23 16:32:24 +0900

    example commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
-import PageContent from '../components/PageContent';
+import PageContent from "../components/PageContent";
 
 function HomePage() {
   return (
     <PageContent title="Welcome!">
       <p>Browse all our amazing events!</p>
+      <p>Great</p>
     </PageContent>
   );
 }
